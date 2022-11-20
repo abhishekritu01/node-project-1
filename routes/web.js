9fa8618a90b906dbc0f0c55672ababf395088353
abhishek <abhishekritu810219@gmail.com>
2022-11-20 16:22:08 +0530

    auth system

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,7 @@
-
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const guest = require('../app/http/middleware/guest')
 
 
 
@@ -9,8 +9,15 @@ function initRoutes(app) {
 
     app.get('/', homeController().index)
    
-    app.get('/login', authController().login)
-    app.get('/register', authController().register)
+    app.get('/login', guest,authController().login)
+    app.post('/login', authController().postLogin)
+
+    app.get('/register',guest, authController().register)
+    app.post('/register', authController().postRegister)  //part-7
+    app.post('/logout', authController().logout)  //part-7
+
+ 
+
     app.get('/cart', cartController().cart)
     app.post('/update-cart', cartController().update)
 }
--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 const PORT = process.env.PORT || 3000
+
+
 
 // Database connection
 const url = 'mongodb://localhost:27017/pizza'
@@ -24,8 +27,6 @@ connection.once('open', () => {
     .on('error', function (err) {
         console.log(err);
     });
-
-
 
 
 // Session store
@@ -44,14 +45,20 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
     // cookie: { maxAge: 1000 * 10} // 24 hour
 }))
- 
+
+
+//passport setting   p7
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 
 app.use(flash())
-//Assets
+//Assets 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: false }))    //handle multi data from front end(url encoded)
 app.use(express.json());
 
 
@@ -61,7 +68,6 @@ app.use((req, res, next) => {
     res.locals.user = req.user
     next()
 })
-
 
 //set Templet Engine
 app.use(expressLayout)
